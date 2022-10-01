f76c5fd29a5e8729ed5de274ac8e3028f9fb704c
dajr592 <dajr592@gmail.com>
2022-10-01 04:39:16 +0000

    last

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*Documentation
- *for 
- *the 
- *programm
+
+/**
+ *main - Entry point
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
