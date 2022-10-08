446bd20f49082eddd911cc04bdf6c691ee50f3f2
dajr592 <dajr592@gmail.com>
2022-10-08 04:04:54 +0000

    betty5

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,27 +1,37 @@
-#include "main.h"
+#include "holberton.h"
 /**
- * print_triangle - prints a triangle, followed by a new line
- * @size: size of the triangle
- */
-void print_triangle(int size)
+  * times_table - Prints the 9 times table, starting with 0
+  *
+  * Return: empty output
+  */
+void times_table(void)
 {
-if (size <= 0)
+int x, y, z, u, d;
+for (x = 0; x <= 9; x++)
 {
+for (y = 0; y <= 9; y++)
+{
+z = x * y;
+if (z > 9)
+{
+u = z % 10;
+d = (z - u) / 10;
+_putchar(44);
+_putchar(32);
+_putchar(d + '0');
+_putchar(u + '0');
+}
+else
+{
+if (y != 0)
+{
+_putchar(44);
+_putchar(32);
+_putchar(32);
+}
+_putchar(z + '0');
+}
+}
 _putchar('\n');
-} else
-{
-int i, j;
-for (i = 1; i <= size; i++)
-{
-for (j = i; j < size; j++)
-{
-_putchar(' ');
-}
-for (j = 1; j <= i; j++)
-{
-_putchar('#');
-}
-_putchar('\n');
-}
 }
 }
